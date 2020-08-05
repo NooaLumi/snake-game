404c5a6e41258a682a3869367c4ad191a4e5163c
Nooa <nooa.lumilaakso@gmail.com>
2020-08-05 16:01:58 +0300

    Added 'hello snake' message

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-
-import Snake from './objects/snake.js';
-import Food from './objects/food.js';
+import Snake from "./objects/snake.js";
+import Food from "./objects/food.js";
 
 const game = {
+	Start() {
+		console.log("Hello Snake!");
+		this.canvas = document.getElementById("canvas");
+		this.ctx = canvas.getContext("2d");
 
-    Start() {
-        this.canvas = document.getElementById("canvas");
-        this.ctx = canvas.getContext("2d");
+		this.gridSize = 20;
+		this.cellSize = this.canvas.width / this.gridSize;
 
-        this.gridSize = 20;
-        this.cellSize = this.canvas.width / this.gridSize;
+		this.snake = new Snake(this.gridSize);
+		this.food = new Food(this.gridSize, this.snake.blocks);
+		this.score = 0;
 
-        this.snake = new Snake(this.gridSize);
-        this.food = new Food(this.gridSize, this.snake.blocks);
-        this.score = 0;
+		document.addEventListener("keydown", (e) =>
+			this.snake.updateDirection(e)
+		);
+		this.lastUpdate = 0; // Keeps track of last gameLoop update
 
-        document.addEventListener('keydown', e => this.snake.updateDirection(e));
-        this.lastUpdate = 0; // Keeps track of last gameLoop update
+		requestAnimationFrame(gameLoop);
+	},
 
-        requestAnimationFrame(gameLoop);
-    },
+	Update() {
+		this.snake.Update();
 
-    Update() {
-        this.snake.Update();
+		if (this.snake.blocks[this.snake.blocks.length - 1] === this.food.pos) {
+			this.snake.eatFood();
+			this.food = new Food(this.gridSize, this.snake.blocks);
+		}
+	},
 
-        if(this.snake.blocks[this.snake.blocks.length - 1] === this.food.pos) {
-            this.snake.eatFood();
-            this.food = new Food(this.gridSize, this.snake.blocks);
-        }
-    },
-
-    Draw() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.food.Draw(this.ctx, this.cellSize);
-        this.snake.Draw(this.ctx, this.cellSize);
-    },
-}
+	Draw() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.food.Draw(this.ctx, this.cellSize);
+		this.snake.Draw(this.ctx, this.cellSize);
+	},
+};
 window.onload = () => game.Start();
 
 function gameLoop(timestamp) {
+	if (timestamp - game.lastUpdate >= 100) {
+		game.Update();
+		game.lastUpdate = timestamp;
+	}
+	game.Draw();
 
-    if((timestamp - game.lastUpdate) >= 100) {
-        game.Update();
-        game.lastUpdate = timestamp;
-    }
-    game.Draw();
-
-    requestAnimationFrame(gameLoop);
+	requestAnimationFrame(gameLoop);
 }
